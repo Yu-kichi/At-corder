10db81c24cf91aa12605b5b30f8acd1a4eabac39
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2022-05-11 23:12:23 +0900

    first commit

---
+++ seiei-10/zentansaku/ABC_068_B.rb
@@ -0,0 +1,11 @@
+number = gets.chomp.to_i
+max =0
+(2..number).each do |num|
+  count = 0
+  while num.even?
+     num = num/2
+     count +=1
+  end
+    max = count if max < count
+end
+puts 2**max
