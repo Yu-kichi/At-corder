b33d8b063e6ba35410963ff4f2bdf9994950c20c
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2022-05-17 23:38:51 +0900

    ABC_250

--- ABC_250/ABC_250_B.rb
+++ ABC_250/ABC_250_B.rb
@@ -1,30 +1,32 @@
-n,a,b = gets.chomp.split.map(&:to_i)
+# frozen_string_literal: true
+
+n, a, b = gets.chomp.split.map(&:to_i)
 ary = []
 ans = []
 
 1.step(n) do |n|
   if n.odd?
     a.times do
-      b.times do 
+      b.times do
         ary << "." * b
         ary << "#" * b
       end
-    ans << ary.join
-    ary = []
+      ans << ary.join
+      ary = []
     end
   else
     a.times do
-      b.times do 
+      b.times do
         ary << "#" * b
         ary << "." * b
       end
-    ans << ary.join
-    ary = []
+      ans << ary.join
+      ary = []
     end
   end
 end
 
 
 ans.each do |n|
-    puts n
+  puts n
 end
