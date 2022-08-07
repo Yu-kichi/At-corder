12d1f07834de4ac6e0bc07bb76eec69f17f198eb
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2022-08-07 23:53:11 +0900

    ABC_173

---
+++ ABC_173/ABC_173_A.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+a = gets.to_i
+while a > 0 do
+  a = a-1000
+end
+p a.abs
---
+++ ABC_173/ABC_173_B.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+a = gets.to_i
+ac = 0
+tle = 0
+wa = 0
+re = 0
+a.times do |n|
+  ab= gets.chomp
+  if ab =="AC"
+    ac +=1
+  elsif ab =="TLE"
+    tle +=1
+  elsif ab =="WA"
+    wa +=1
+  else
+    re+=1
+  end
+end
+puts "AC x #{ac}"
+puts "WA x #{wa}"
+puts "TLE x #{tle}"
+puts "RE x #{re}"
